ca42429419ac734284c6b20ca7a457237aecfba5
Nguyen <nguyenc2300110@student.ctu.edu.vn>
2026-04-27 18:01:38 +0700

    add identity dep to app module

--- be/app/src/main/java/com/flix/app/FlixPlaftformApplication.java
+++ be/app/src/main/java/com/flix/app/FlixPlaftformApplication.java
@@ -3,7 +3,7 @@ package com.flix.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication( scanBasePackages = "com.flix")
 public class FlixPlaftformApplication {
 
     static void main(String[] args) {
---
+++ be/app/src/main/java/com/flix/app/api/PingAPI.java
@@ -0,0 +1,13 @@
+package com.flix.app.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/health")
+public class PingAPI {
+
+    @GetMapping
+    public String ping() {
+        return "pong";
+    }
+}
